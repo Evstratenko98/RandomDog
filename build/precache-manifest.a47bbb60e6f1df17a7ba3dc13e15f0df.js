61272b45fec4f881cacbb9f5e3995dc8c4f69c09
Evstratenko98 <evstratenko.sv98@gmail.com>
2020-10-21 19:28:42 +0400

    Node App

---
+++ build/precache-manifest.a47bbb60e6f1df17a7ba3dc13e15f0df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0868ad81ae8c68bf0b342c0381dc1aef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc8ab7b562af57bebe61",
+    "url": "/static/css/main.f64ed36b.chunk.css"
+  },
+  {
+    "revision": "c8448668fa2736c33cfa",
+    "url": "/static/js/2.5d6ad937.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d6ad937.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc8ab7b562af57bebe61",
+    "url": "/static/js/main.de56851c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
